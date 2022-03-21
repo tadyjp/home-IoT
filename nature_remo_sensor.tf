6022de31bdcaa191eccd462ac9c93b26701c7aac
Masato TADA <a.dat.jp@gmail.com>
2022-03-21 22:54:33 +0900

    add assume_role_policy

--- nature_remo_sensor.tf
+++ nature_remo_sensor.tf
@@ -52,6 +52,20 @@ resource "aws_lambda_function" "nature_remo_to_dynamo" {
 resource "aws_iam_role" "nature_remo_to_dynamo" {
   name = "nature-remo-to-dynamo"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
   inline_policy {
     name = "inline-policy"
 
