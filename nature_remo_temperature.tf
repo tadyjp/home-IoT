8672d712299df2c7e57a753b82f169713ba66bce
Masato TADA <a.dat.jp@gmail.com>
2022-03-21 21:27:04 +0900

    dynamodb

---
+++ nature_remo_temperature.tf
@@ -0,0 +1,37 @@
+resource "aws_dynamodb_table" "nature_remo_sensor" {
+  name         = "nature-remo-sensor"
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "DeviceID"
+    type = "S"
+  }
+
+  attribute {
+    name = "Timestamp"
+    type = "N"
+  }
+
+  attribute {
+    name = "Temperature"
+    type = "N"
+  }
+
+  attribute {
+    name = "Humidity"
+    type = "N"
+  }
+
+  attribute {
+    name = "Illuminance"
+    type = "N"
+  }
+
+  attribute {
+    name = "Motion"
+    type = "N"
+  }
+
+  hash_key  = "UserId"
+  range_key = "GameTitle"
+}
