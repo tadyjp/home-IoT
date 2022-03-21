34d6273e7de0132fff4a07a1a4c35e9ab2cd98b9
Masato TADA <a.dat.jp@gmail.com>
2022-03-21 21:53:31 +0900

    fix unused attr

--- nature_remo_temperature.tf
+++ nature_remo_temperature.tf
@@ -12,26 +12,6 @@ resource "aws_dynamodb_table" "nature_remo_sensor" {
     type = "N"
   }
 
-  attribute {
-    name = "Temperature"
-    type = "N"
-  }
-
-  attribute {
-    name = "Humidity"
-    type = "N"
-  }
-
-  attribute {
-    name = "Illuminance"
-    type = "N"
-  }
-
-  attribute {
-    name = "Motion"
-    type = "N"
-  }
-
   hash_key  = "DeviceID"
   range_key = "Timestamp"
 }
