77d24c61941c515c252fa2b45c22114d899ad6ea
tady (GitHub) <a.dat.jp@gmail.com>
2022-04-24 17:26:33 +0900

    Nature API cron 実行 (#5)
    * cron
    
    * rm dist
    
    * build
    
    * npm run

---
+++ lambda/nature_remo_to_dynamo/src/index.ts
@@ -0,0 +1,126 @@
+import * as AWS from "aws-sdk";
+import fetch from "node-fetch";
+import { ScheduledEvent } from "aws-lambda";
+
+const dynamoTable = "nature-remo-sensor";
+const natureAPIToken = process.env.NATURE_API_TOKEN!;
+
+class SensorData {
+  constructor(
+    readonly serialNumber: string,
+    readonly eventType: "temperature" | "humidity" | "illuminance" | "motion" | "coefficient" | "cumulative_electric_energy_effective_digits" | "normal_direction_cumulative_electric_energy" | "cumulative_electric_energy_unit" | "reverse_direction_cumulative_electric_energy" | "measured_instantaneous",
+    readonly timestamp: Date,
+    readonly value: number,
+  ) {}
+}
+
+class NatureAPIService {
+  natureAPIToken: string;
+  natureDevicesURL = "https://api.nature.global/1/devices";
+  natureAppliancesURL = "https://api.nature.global/1/appliances";
+
+  constructor(natureAPIToken: string){
+    this.natureAPIToken = natureAPIToken
+  }
+
+  async fetchSensorsData(): Promise<SensorData[]> {
+    const response = await fetch(this.natureDevicesURL, {
+      method: "GET",
+      headers: { "Authorization": `Bearer ${this.natureAPIToken}` },
+    });
+
+    if (response.status !== 200) {
+      throw new Error("failed to get nature api devices: status=" + response.status);
+    }
+
+    const body = await response.json() as any[];
+    console.log("fetchSensorsData", body);
+
+    const list : SensorData[] = [];
+
+    for (const item of body) {
+      if(item.newest_events?.te) {
+        list.push(new SensorData(item.serial_number, "temperature", new Date(item.newest_events.te.created_at), item.newest_events.te.value));
+      }
+      if(item.newest_events?.hu) {
+        list.push(new SensorData(item.serial_number, "humidity", new Date(item.newest_events.hu.created_at), item.newest_events.hu.value));
+      }
+      if(item.newest_events?.il) {
+        list.push(new SensorData(item.serial_number, "illuminance", new Date(item.newest_events.il.created_at), item.newest_events.il.value));
+      }
+      if(item.newest_events?.mo) {
+        list.push(new SensorData(item.serial_number, "motion", new Date(item.newest_events.mo.created_at), item.newest_events.mo.value));
+      }
+    }
+
+    return list
+  }
+
+  async fetchSmartMeterData(): Promise<SensorData[]> {
+    const response = await fetch(this.natureAppliancesURL, {
+      method: "GET",
+      headers: { "Authorization": `Bearer ${this.natureAPIToken}` },
+    });
+
+    if (response.status !== 200) {
+      throw new Error("failed to get nature api appliances: status=" + response.status);
+    }
+
+    const body = await response.json() as any[];
+    console.log("fetchSmartMeterData", body);
+
+    const list : SensorData[] = [];
+
+    for (const item of body) {
+      if (!item.smart_meter) {
+        continue;
+      }
+
+      for(const property of item.smart_meter.echonetlite_properties) {
+        list.push(new SensorData(item.device.serial_number, property.name, new Date(property.updated_at), Number(property.val)));
+      }
+    }
+
+    return list
+  }
+}
+
+function dynamoPutItem(dynamoDB: AWS.DynamoDB, dynamoTable: string, sensorData : SensorData) {
+  const params = {
+    TableName: dynamoTable,
+    Item: {
+      SensorType: { S: `${sensorData.serialNumber}#${sensorData.eventType}` },
+      Timestamp: { N: (sensorData.timestamp.getTime() / 1000).toString() },
+      Value: { N: sensorData.value.toString() },
+    },
+  }
+
+  return new Promise((resolve, reject) => {
+    dynamoDB.putItem(params, function (err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve("ok");
+      }
+    });
+  });
+}
+
+async function processEvents(dynamoDB: AWS.DynamoDB, dynamoTable: string, natureAPIService: NatureAPIService){
+  const sensorDataList = await natureAPIService.fetchSensorsData()
+  for (const sensorData of sensorDataList) {
+    dynamoPutItem(dynamoDB, dynamoTable, sensorData);
+  }
+
+  const smartMeterDataList = await natureAPIService.fetchSmartMeterData()
+  for (const sensorData of smartMeterDataList) {
+    dynamoPutItem(dynamoDB, dynamoTable, sensorData);
+  }
+}
+
+export async function handler(event: ScheduledEvent){
+  const dynamo = new AWS.DynamoDB();
+  const natureAPIService = new NatureAPIService(natureAPIToken);
+
+  processEvents(dynamo, dynamoTable, natureAPIService)
+};
