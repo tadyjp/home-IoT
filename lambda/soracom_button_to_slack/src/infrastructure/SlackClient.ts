2d994669d844db73a3dedc4a1cdff2ce32b25e3a
tady (GitHub) <a.dat.jp@gmail.com>
2022-04-24 22:29:12 +0900

    Feature/nature sensor (#6)
    * cron
    
    * rm dist
    
    * build
    
    * npm run
    
    * rm node-fetch

--- lambda/soracom_button_to_slack/src/infrastructure/SlackClient.ts
+++ lambda/soracom_button_to_slack/src/infrastructure/SlackClient.ts
@@ -1,4 +1,4 @@
-import fetch from "node-fetch";
+import { post } from "../utils/https";
 import { IMessageClient } from "../application/IMessageClient";
 import { MessageEvent } from "../entity/MessageEvent";
 
@@ -6,32 +6,32 @@ export class SlackClient implements IMessageClient {
   constructor(private webhookURL: string) {}
 
   async post(event: MessageEvent): Promise<void> {
-    const response = await fetch(this.webhookURL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        text: "ポストに投函されたよ :mailbox_with_mail:",
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "ポストに投函されたよ :mailbox_with_mail:",
+    await post(
+      this.webhookURL,
+      {},
+      JSON.stringify({
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          text: "ポストに投函されたよ :mailbox_with_mail:",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "ポストに投函されたよ :mailbox_with_mail:",
+              },
             },
-          },
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "```" + event.raw.printMarkdown() + "```",
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "```" + event.raw.printMarkdown() + "```",
+              },
             },
-          },
-        ],
-      }),
-    });
-
-    if (response.status !== 200) {
-      throw new Error("failed to post to slack: status=" + response.status);
-    }
+          ],
+        }),
+      })
+    );
   }
 }
